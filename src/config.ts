12568469b650024c9a2839cc2497251a98c8de55
Ryan Maber <ryan@ryanmaber.com>
2023-07-11 23:11:29 +0100

    Update endpoint url

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = "https://au5e5odkot5bxvchjsyvuy3h4e0ajiap.lambda-url.eu-west-2.on.aws";
+export const API_URL = "https://5g4fctc7ysvibfijbbsg2hcqfm0utifb.lambda-url.eu-west-2.on.aws";
